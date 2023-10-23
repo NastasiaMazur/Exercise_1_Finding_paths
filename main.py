fcba387342bb71381b29f3a46e6a6088e5184bfa
NastasiaMazur <nvmazour@mail.ru>
2023-10-23 11:26:30 +0200

    phase one

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("Ex1")
+
+
+# Phase 1:
+def print_labyrinth(lab: list[str]):
+ rows = len(lab)
+ columns = len(lab[0])
+
+ # Prints the top row with column numbers
+ top_row = " " + "".join([str(i % 10) for i in range(columns)]) + " "
+ print(top_row)
+
+ for i in range(rows):
+  row = str(i) + lab[i] + str(i)
+  print(row)
+
+ # Prints the bottom row with column numbers
+ bottom_row = " " + "".join([str(i % 10) for i in range(columns)]) + " "
+ print(bottom_row)
+
+
+labyrinth = [
+ "███████",
+ "█     █",
+ "█   ███",
+ "█ ███ █",
+ "█     █",
+ "███████",
+]
+
+print_labyrinth(labyrinth)
+
+
+
+
+
+
+
+"""
+
+labyrinth = [
+ "███████",
+ "█     █",
+ "█   ███",
+ "█ ███ █",
+ "█     █",
+ "███████",
+]
+
+#print(labyrinth)
+print_labyrinth(labyrinth)
+"""
