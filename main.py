48f028a57ff93570aca0b9c808e9b78f67909f22
NastasiaMazur <nvmazour@mail.ru>
2023-10-25 12:00:13 +0200

    Phase three

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def print_labyrinth(lab: list[str]):
  bottom_row = " " + "".join([str(i % 10) for i in range(columns)]) + " "
  print(bottom_row)
 
-
 labyrinth = [
  "███████",
  "█     █",
@@ -61,6 +60,62 @@ end = prompt_user_for_location("end")
 
 print("Start location:", start)
 print("End location:", end)
+
+
+# Phase 3
+
+from collections import deque
+def bfs(lab: list[str], start: tuple[int, int], end: tuple[int, int]) -> list[tuple[int, int]]:
+ # Define the possible moves: up, down, left, right
+ moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+ # Initialize a queue to perform BFS
+ queue = deque()
+
+ # Initialize a set to keep track of visited locations
+ visited = set()
+
+ # Initialize a dictionary to store the path
+ path = {}
+
+ # Add the start location to the queue
+ queue.append(start)
+ visited.add(start)
+
+ while queue:
+  current = queue.popleft()
+
+  if current == end:
+   # Build and return the path
+   path_list = [current]
+   while current != start:
+    current = path[current]
+    path_list.append(current)
+   path_list.reverse()
+   return path_list
+
+  for move in moves:
+   new_location = (current[0] + move[0], current[1] + move[1])
+
+   if 0 <= new_location[0] < len(lab) and 0 <= new_location[1] < len(lab[0]) and lab[new_location[0]][
+    new_location[1]] == ' ' and new_location not in visited:
+    queue.append(new_location)
+    visited.add(new_location)
+    path[new_location] = current
+
+ return None
+
+
+# Example usage:
+path = bfs(labyrinth, start, end)
+if path:
+ print("Path from start to end:")
+ for location in path:
+  print(location)
+else:
+ print("No path found.")
+
+
 
 """
 
